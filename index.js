4189fa34fd755c0fcf10ebf77be4eb4d985ec29f
aboynamedcrow <56894461+aboynamedcrow@users.noreply.github.com>
2022-04-07 03:59:42 +0900

    asdf

--- index.js
+++ index.js
@@ -20,3 +20,19 @@ function showDropdown() {
       }
     }
   }
+
+  $(document).ready(function() {
+  
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 209) {
+        $('.navbar').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < 210) {
+        $('.navbar').removeClass('navbar-fixed');
+      }
+    });
+  });
