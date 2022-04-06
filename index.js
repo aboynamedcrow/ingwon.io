0226485049dba7679211d40299b6253f21b722f7
aboynamedcrow <56894461+aboynamedcrow@users.noreply.github.com>
2022-04-07 04:16:11 +0900

    asdf

--- index.js
+++ index.js
@@ -28,10 +28,11 @@ function showDropdown() {
         //.log to determine when you want the 
         //nav bar to stick.  
         console.log($(window).scrollTop())
-      if ($(window).scrollTop() > 249) {
+      var height = $('#business-card').height();
+      if ($(window).scrollTop() > height+5) {
         $('.navbar').addClass('navbar-fixed');
       }
-      if ($(window).scrollTop() < 242) {
+      if ($(window).scrollTop() < height+4) {
         $('.navbar').removeClass('navbar-fixed');
       }
     });
