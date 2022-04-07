e0733ae534a7bb789d88421e3ec18cc6c2ea072c
aboynamedcrow <56894461+aboynamedcrow@users.noreply.github.com>
2022-04-07 17:14:07 +0900

    asdf

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+
+//NavBar Functions
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function showDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+ $(document).ready(function() {
+  
+    $(window).scroll(function () {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+      var height = $('#business-card').height();
+      if ($(window).scrollTop() > height+5) {
+        $('.navbar').addClass('navbar-fixed');
+      }
+      if ($(window).scrollTop() < height+4) {
+        $('.navbar').removeClass('navbar-fixed');
+      }
+    });
+  /*});
+
 //Toggle Dark Mode via localStorage
 $(document).ready(function() {
     var darkSwitch = document.querySelector(".dark-checkbox");
@@ -38,44 +78,3 @@ $(document).ready(function() {
         console.log(darkSwitch);
     });
 });
-
-
-
-//NavBar Functions
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function showDropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-
- /*$(document).ready(function() {*/
-  
-    $(window).scroll(function () {
-        //if you hard code, then use console
-        //.log to determine when you want the 
-        //nav bar to stick.  
-        console.log($(window).scrollTop())
-      var height = $('#business-card').height();
-      if ($(window).scrollTop() > height+5) {
-        $('.navbar').addClass('navbar-fixed');
-      }
-      if ($(window).scrollTop() < height+4) {
-        $('.navbar').removeClass('navbar-fixed');
-      }
-    });
-  /*});*/
