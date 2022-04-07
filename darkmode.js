a281504529b47724ae9f37aefbbe56ab0f42b2a6
aboynamedcrow <56894461+aboynamedcrow@users.noreply.github.com>
2022-04-07 17:38:58 +0900

    asdf

---
+++ darkmode.js
@@ -0,0 +1,41 @@
+
+//Toggle Dark Mode via localStorage
+/*$(document).ready(function() {*/
+    var darkSwitch = document.querySelector(".dark-checkbox");
+
+    console.log(darkSwitch);
+
+    const currentTheme = localStorage.getItem("theme");
+
+    console.log(currentTheme);
+
+    if (currentTheme == "dark") {
+    document.body.classList.add("dark-theme");
+    }
+
+    darkSwitch.addEventListener("change", function () {
+
+        let theme = "dark";
+        console.log(darkSwitch.checked);
+
+        if(this.checked) {
+            console.log(darkSwitch.checked);
+            theme = "dark";
+            if (document.body.classList.contains("dark-theme")){
+                theme = "dark";
+            } else {
+                document.body.classList.add("dark-theme");
+            }}
+            else {
+            console.log(darkSwitch.checked);
+            theme = ""
+                if (document.body.classList.contains("dark-theme")){
+                    document.body.classList.toggle("dark-theme");
+                } else {
+                    theme = "";
+                }
+            }
+        localStorage.setItem("theme", theme);
+        console.log(darkSwitch);
+    });
+//});
